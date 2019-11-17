4e2c37e6d4286fed948d6b56f6244576d33fac53
Denis Potapov <fantom.r45@mail.ru>
2019-11-17 17:00:19 +0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        Student f = new Student();
+        f.setName("Иван");
+        f.setSurname("Иванов");
+        f.setPatronymic("Иванович");
+        String name = f.getName();
+        String surname = f.getSurname();
+        String patronymic = f.getPatronymic();
 
+        f.printFullName();
 
 
 
